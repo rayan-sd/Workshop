4cadd729450846836afb23be178db2691d405124
rayan soued <rsoued@localhost.localdomain>
2021-03-04 11:10:00 +0100

    [ADD2]

--- src/tool.c
+++ src/tool.c
@@ -35,6 +35,7 @@ static const char *STUDENT_SENTENCES[] = {
     "Y'aura un deuxieme BTTF ?",
     "J'ai un bug que j'arrive pas à résoudre, c'est urgent",
     "J'ai une question ?",
+    "Super! j'ai crée une branche,"
 };
 
 const char *get_sentence(size_t index)
